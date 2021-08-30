2698adfa4a4d088e5b700143deeff61d3dad52e1
Vellone <fabriciovellone@gmail.com>
2021-08-30 23:24:44 +0200

    Add swagger to document endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 import router from "./routes";
 
 import "./database";
 import "./shared/container";
+import swaggerFile from "./swagger.json";
 import { AppError } from "./errors/AppError";
 
 const app = express();
-
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 app.use((err: Error, request: Request, response: Response, next: NextFunction): Response => {
   if (err instanceof AppError) {
